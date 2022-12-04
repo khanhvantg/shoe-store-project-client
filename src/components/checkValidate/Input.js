293f0af873fe50ad4073f11f3771f1de0c4fb1b5
khanhvantg <khanhvanvippro2001@gmail.com>
2022-12-05 00:49:08 +0700

    05/12/2022

--- src/components/checkValidate/Input.js
+++ src/components/checkValidate/Input.js
@@ -20,7 +20,7 @@ const getRegExp = (type) => {
       regx = /^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|100)$/;
       break;
     case 'NUMBER':
-      regx = /^(?<!-)(?<!\d)[1-9][0-9]*$/;
+      regx = /^(?!-)(?!d)[1-9][0-9]*$/;
       break;
     default:
       break;
