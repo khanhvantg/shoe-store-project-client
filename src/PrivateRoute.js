6a68770d57da23a07353c73f09db14897f4e2beb
khanhvantg <khanhvanvippro2001@gmail.com>
2023-05-26 11:41:05 +0700

    26/05/2023

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -5,6 +5,7 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
+    console.log(children)
     if (children.type.name==="SignUp" || children.type.name==="ForgetPassword")
         return userInfo ? < Navigate to="/" /> : children;
     else return userInfo ? children : < Navigate to="/login" />;
